d81aa8e34e6681e4763d49eb838df5bbe18c30bc
zhangdz <zdz408@126.com>
2022-09-07 02:32:34 +0000

    update main.py.
    Signed-off-by: zhangdz <zdz408@126.com>

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def handler(event, context):
 
 
 def checkIn():
-    COOKIES = os.getenv('COOKIES')
+    #COOKIES = os.getenv('COOKIES')
+    COOKIES = '_ga=GA1.2.1471779015.1662441243; _gid=GA1.2.2081561511.1662441243; koa:sess=eyJ1c2VySWQiOjEyNTc5NCwiX2V4cGlyZSI6MTY4ODM2MTQwNzg3OSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=ca7_5AKGh3-L1NjGxuw6loR6XSw; __cf_bm=RFgwjpe4yqWWMgPLIUM0CuUfPzGAfEQ_jJhOySn7RV4-1662516383-0-AWQ1u9nCd6i2a5nT4xSNIj2CC/HwXwCE0hk7wgxHW3z8EJ/xCjqPn3eRT/RimBP9FqGz/4kgN40j+GU0rxFIwSJXSRBUqVgBIBm7BxcYHrhlWFbrgJEIZiX9K2Nb4ibzKw=='
     BOT_TOKEN = os.getenv('BOT_TOKEN')
     CHAT_ID = os.getenv('CHAT_ID')
     if COOKIES is None:
